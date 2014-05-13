e97e4fc88ab88cf7f9019dfa1cbfba892d184c07
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-05-13 13:12:15 +0200

    create helper for images

--- src/test/cpp/permutation_unittest.cpp
+++ src/test/cpp/permutation_unittest.cpp
@@ -1,6 +1,11 @@
 #include <vector>
+#include <cstdarg>
 #include "gtest/gtest.h"
 #include "../../main/cpp/permutation.hpp"
+
+#include <iostream>
+
+std::vector<unsigned int> create_image(unsigned int points, ...);
 
 static const unsigned int max_base = 100;
 
@@ -13,10 +18,7 @@ TEST(PermutationTest, ImageIdentity) {
 }
 
 TEST(PermutationTest, Image10) {
-  std::vector<unsigned int> image;
-  image.push_back(1);
-  image.push_back(0);
-  Permutation p = Permutation(image);
+  Permutation p = Permutation(create_image(2, 1, 0));
 
   EXPECT_EQ(1, p(0));
   EXPECT_EQ(0, p(1));
@@ -25,4 +27,16 @@ TEST(PermutationTest, Image10) {
   for (unsigned int n = 2; n < max_base; ++n) {
     EXPECT_EQ(n, p(n));
   }
+}
+
+std::vector<unsigned int> create_image(unsigned int points, ...) {
+  std::vector<unsigned int> image;
+  va_list arguments;
+  va_start(arguments, points);
+  while (points--) {
+    unsigned int value = va_arg(arguments, unsigned int);
+    image.push_back(value);
+  }
+  va_end(arguments);
+  return image;
 }
